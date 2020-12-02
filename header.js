7b4dc4854434ed9138237af6f3d47b5bc1b91cda
chokcc <40905206+chokcc@users.noreply.github.com>
2020-12-02 14:28:03 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,6 +1,7 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-19525207-1']);
 _gaq.push(['_trackPageview']);
+onClick=”_gaq.push([‘_trackEvent’, ‘category’, ‘action’, ‘opt_label’, opt_value, opt_noninteraction]);”
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
