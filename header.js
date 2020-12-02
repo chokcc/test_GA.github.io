cd1474aaff00e01284563ec18044ad36c4e4838d
chokcc <40905206+chokcc@users.noreply.github.com>
2020-12-02 14:49:19 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-19525207-1']);
 _gaq.push(['_trackPageview']);
-onClick=”_gaq.push([‘_trackEvent’, ‘category’, ‘action’, ‘opt_label’, opt_value, opt_noninteraction]);”
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+onClick=”_gaq.push([‘_trackEvent’, ‘category’, ‘action’, ‘opt_label’, opt_value, opt_noninteraction]);”
