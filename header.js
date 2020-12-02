296faba796137248beafc383805e3309fd53ffc9
chokcc <40905206+chokcc@users.noreply.github.com>
2020-12-02 15:37:30 +0700

    Update header.js

--- header.js
+++ header.js
@@ -8,11 +8,11 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 })();
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'G-T3V50TZQ0Z']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+<!-- Global site tag (gtag.js) - Google Analytics -->
+async src="https://www.googletagmanager.com/gtag/js?id=G-T3V50TZQ0Z">
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T3V50TZQ0Z');
