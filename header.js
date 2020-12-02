6c998f5bac06a09e10eee0c1e306155527653f10
chokcc <40905206+chokcc@users.noreply.github.com>
2020-12-02 13:16:02 +0700

    ga on js

---
+++ header.js
@@ -0,0 +1,29 @@
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+async src="https://www.googletagmanager.com/gtag/js?id=G-T3V50TZQ0Z">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T3V50TZQ0Z');
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+async src="https://www.googletagmanager.com/gtag/js?id=UA-19525207-1">
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-19525207-1');
+
+
+
+
+
+
+
+
+
+
+
