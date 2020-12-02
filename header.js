7afff50e1b8ad0a59101b7b99d2ae5dcf4b8d7ec
chokcc <40905206+chokcc@users.noreply.github.com>
2020-12-02 13:21:24 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,29 +1,8 @@
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-async src="https://www.googletagmanager.com/gtag/js?id=G-T3V50TZQ0Z">
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-T3V50TZQ0Z');
-
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-async src="https://www.googletagmanager.com/gtag/js?id=UA-19525207-1">
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-19525207-1');
-
-
-
-
-
-
-
-
-
-
-
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-19525207-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
